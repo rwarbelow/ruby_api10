44d49849b51745b1f3e48e1de0973106dd0c094b
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-31 14:41:58 -0500

    testing response text

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,12 +9,6 @@ post '/send_message' do
   erb :index
 end
 
-post '/call' do
-  myMessage = Message.new(params[:number])
-  myMessage.call
-  erb :index
-end
-
 get '/sent_messages' do
   myMessages = Message.new
   @allmessages = myMessages.get_sent_messages
@@ -29,26 +23,6 @@ get '/received_messages' do
   erb :all_received_messages
 end
 
-get '/voice/?' do
-  response = Twilio::TwiML::Response.new do |r|
-    r.Say 'Congratulations! You\'ve successfully deployed ' \
-          'the Twilio HackPack for Heroku and Sinatra!', :voice => 'woman'
-  end
-  response.text
-end
-
-def get_or_post(path, opts={}, &block)
-  get(path, opts, &block)
-  post(path, opts, &block)
-end
-
-# # Home page and reference
-# get '/' do
-#   @title = "Home"
-#   erb :home
-# end
-
-# Voice Request URL
 get '/voice_mail' do
   response = Twilio::TwiML::Response.new do |r|
     r.Say 'Congratulations! You\'ve successfully deployed ' \
@@ -61,7 +35,7 @@ end
 get '/sms-quickstart' do
   p params
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Texted from #{params["From"]}, with #{params["Body"]}. "
+    r.Sms "Changed the response text."
   end 
   twiml.text
 end
