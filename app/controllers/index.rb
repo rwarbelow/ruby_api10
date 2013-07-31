549ea8aaf09fccff46aaebca6a8eee8395ed2687
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-31 13:25:38 -0500

    Stuffs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -60,11 +60,10 @@ end
 # SMS Request URL
 get '/sms-quickstart' do
   p params
-  # p "FIRST ONE"
-  # twiml = Twilio::TwiML::Response.new do |r|
-  #   r.Sms "Hey Monkey. Thanks for the message!"
-  # end 
-  # twiml.text
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Sms "Texted from #{params["From"]}, with #{params["Body"]}. "
+  end 
+  twiml.text
 end
 
 post '/rachel' do 
