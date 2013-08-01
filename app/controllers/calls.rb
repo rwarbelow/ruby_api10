26da032aac18d4f3012a1bbf99abb9a47e8fab69
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-31 20:26:53 -0500

    OMG!

--- app/controllers/calls.rb
+++ app/controllers/calls.rb
@@ -6,9 +6,9 @@ get '/hello-monkey' do
     '+14158675312' => 'Marcel',
   }
 
-  name = Parent.find_by_cell_phone([params['From']]) || 'Guest'
+  @caller = Parent.find_by_cell_phone([params['From']]) || 'Guest'
   Twilio::TwiML::Response.new do |r|
-    r.Say "Hello #{name}"
+    r.Say "Hello #{@caller.first_name}"
     r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
     r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
       g.Say 'To speak to a real monkey, press 1.'
