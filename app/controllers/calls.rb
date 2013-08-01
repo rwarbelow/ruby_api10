2d8aa09a4f1a7932c013ce79ab18b65280974886
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-31 21:58:50 -0500

    put logic back in controller

--- app/controllers/calls.rb
+++ app/controllers/calls.rb
@@ -1,16 +1,13 @@
 get '/hello-monkey' do
-
-  Twil.incoming_call
-
-  # @caller = Parent.find_by_cell_phone([params['From']]) || 'Guest'
-  # Twilio::TwiML::Response.new do |r|
-  #   r.Say "Hello #{@caller.first_name}"
-  #   r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
-  #     g.Say 'I cannot believe I programmed this.'
-  #     g.Say 'Push a key. One or two or three.'
-  #     g.Say 'But you can also press any other key to start over.'
-  #   end
-  # end.text
+  @caller = Parent.find_by_cell_phone([params['From']]) || 'Guest'
+  Twilio::TwiML::Response.new do |r|
+    r.Say "Hello #{@caller.first_name}"
+    r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
+      g.Say 'I cannot believe I programmed this.'
+      g.Say 'Push a key. One or two or three.'
+      g.Say 'But you can also press any other key to start over.'
+    end
+  end.text
 end
  
 get '/hello-monkey/handle-gather' do
