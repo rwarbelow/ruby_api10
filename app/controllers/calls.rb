dccd8986e10b447aeae21c1328a63832e6322f0c
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-07-31 22:20:24 -0500

    testing call function

--- app/controllers/calls.rb
+++ app/controllers/calls.rb
@@ -4,12 +4,13 @@ get '/hello-monkey' do
     r.Say "Hello #{@caller.first_name}"
     r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
       g.Say 'I cannot believe I programmed this.'
-      g.Say 'Push a key. One or two or three.'
+      g.Say 'Push a key. One will read you a little message. Two will let you record yourself so you can listen to yourself talk. Three will let you record a voicemail that I will receive.'
       g.Say 'But you can also press any other key to start over.'
     end
   end.text
 end
  
+
 get '/hello-monkey/handle-gather' do
   redirect '/hello-monkey' unless ['1', '2', '3'].include?(params['Digits'])
   if params['Digits'] == '1'
@@ -19,7 +20,7 @@ get '/hello-monkey/handle-gather' do
     end
   elsif params['Digits'] == '2'
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Record something. I do not think I will be able to receive it because I am not a good programmer yet. Press pound when you are finished. Leave me a really good message in case I can get it. Ok?'
+      r.Say 'Record something. There is no point really to this other than for you to hear your own voice. Press pound when you are finished.'
       r.Record :maxLength => '30', :action => '/hello-monkey/handle-record', :method => 'get'
     end
   elsif params['Digits'] == '3'
@@ -27,12 +28,16 @@ get '/hello-monkey/handle-gather' do
   end
   response.text
 end
- 
- 
+
+
 get '/hello-monkey/handle-record' do
   Twilio::TwiML::Response.new do |r|
     r.Say 'Listen to your message.'
     r.Play params['RecordingUrl']
     r.Say 'Goodbye.'
   end.text
+end
+
+post '/call' do
+  myMessage = Message.create(params[:numbers])
 end
